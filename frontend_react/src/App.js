f371cb1c61c9998b76059dd755554edf0f0b55d7
jaresj <justice.jares@gmail.com>
2022-11-30 13:15:17 -0800

    added navbar

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import './App.scss'
+import { About, Footer, Header, Skills, Testimonials, Work } from './container';
+import { Navbar } from './components';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonials />
+        <Footer />
+    </div>
   );
 }
 
